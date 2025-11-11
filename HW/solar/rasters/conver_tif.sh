ac94dad25fb3ffc1293a16d5e7245615d0becca8
M.A. Ponce-Pacheco <map10194@nyu.edu>
2025-11-10 22:45:08 -0500

    Apply .gitignore.

--- HW/solar/rasters/conver_tif.sh
+++
@@ -1,8 +0,0 @@
-cd ./skyfactor
-mkdir -p ../svf_tifs
-
-for f in *.sgrd; do
-  base=$(basename "$f" .sgrd)
-  echo "Convirtiendo $f → ../svf_tifs/${base}.tif"
-  saga_cmd io_gdal 1 -GRIDS "$f" -FILE "../svf_tifs/${base}.tif"
-done
